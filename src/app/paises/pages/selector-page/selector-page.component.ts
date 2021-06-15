6e681ae716054fbf1f26443a6ce0399528fdc098
ConxiCr <conxicr@hotmail.com>
2021-06-15 11:27:33 +0200

    selector por pais
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
 
+import { PaisSmall } from '../../interfaces/paises.interface';
+
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -11,17 +13,32 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.formBuilder.group({
-    region: ['', Validators.required ]
+    region: ['', Validators.required ],
+    pais: ['', Validators.required ],
 
   })
   //llenar selectores
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
 
   constructor( private formBuilder: FormBuilder,
-               private PaisesService: PaisesService ) { }
-
+               private paisesService: PaisesService ) { }
+  
   ngOnInit(): void {
-    this.regiones = this.PaisesService.regiones;
+    this.regiones = this.paisesService.regiones;
+
+    //cuando cambie la región
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe( region => {
+        console.log(region)
+
+        this.paisesService.getPaisesPorRegion( region )
+         .subscribe( paises => {
+           console.log(paises)
+           this.paises = paises;
+         })
+      })
+
   }
 
   guardar(){

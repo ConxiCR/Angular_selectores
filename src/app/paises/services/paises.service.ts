e2f890da719ef7ab27c07a2da27c699d84f0dd45
ConxiCr <conxicr@hotmail.com>
2021-06-15 11:24:57 +0200

    llamada a la http
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { PaisSmall } from '../interfaces/paises.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
   
+  private baseUrl: string = 'https://restcountries.eu/rest/v2'
   //En REST Countries no hay un endpoint para cargar. Lo hacemos manual
   private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   //regiones de tipo publico. Con un arreglo desestructurado
@@ -12,5 +17,11 @@ export class PaisesService {
     return [ ...this._regiones ];
   }
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getPaisesPorRegion( region:string ): Observable<PaisSmall[]> {
+  
+  const url: string = `${ this.baseUrl}/region/${ region }?fields=alpha3Code;name`
+  return this.http.get<PaisSmall[]> ( url );
+  }
 }

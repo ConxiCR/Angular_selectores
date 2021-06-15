3ebc1be92f3f12722906714bcf994a2981e739b0
ConxiCr <conxicr@hotmail.com>
2021-06-15 08:45:44 +0200

    llamamos a los paises manualmente
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PaisesService {
+  
+  //En REST Countries no hay un endpoint para cargar. Lo hacemos manual
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  //regiones de tipo publico. Con un arreglo desestructurado
+  get regiones(): string[] {
+    return [ ...this._regiones ];
+  }
 
   constructor() { }
 }
